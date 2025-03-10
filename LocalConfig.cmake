5160025198cb2c848a120bc3c9be7c632dca80e9
Sebastian Mihnea <49305133+MrSebi06@users.noreply.github.com>
2025-03-10 11:12:45 +0100

    Update LocalConfig.cmake

--- LocalConfig.cmake
+++ LocalConfig.cmake
@@ -1,4 +1,6 @@
-﻿# Exemple common path, change it to your own path
+# ! DO NOT COMMIT THIS FILE !
+ 
+ # Exemple common path, change it to your own path
 # If on windows, you can use ${USER_HOME_DIRECTORY} instead of $ENV{HOME}
 add_subdirectory($ENV{HOME}/Repositories/OpenGL/common ${CMAKE_BINARY_DIR}/common_build)
 target_link_libraries(${PROJECT_NAME} PRIVATE common)
